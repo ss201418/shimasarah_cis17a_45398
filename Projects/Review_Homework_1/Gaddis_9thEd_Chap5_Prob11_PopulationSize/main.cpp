79f82cc83b042b78d70c0ba2a55a1e7ee1a4bd15
Sarah S <sarblue201418@gmail.com>
2021-06-26 03:42:10 -0700

    Signed-off-by: Sarah S <sarblue201418@gmail.com>

--- Projects/Review_Homework_1/Gaddis_9thEd_Chap5_Prob11_PopulationSize/main.cpp
+++ Projects/Review_Homework_1/Gaddis_9thEd_Chap5_Prob11_PopulationSize/main.cpp
@@ -20,7 +20,8 @@ using namespace std;
 int main(int argc, char** argv) {    
     //Declare all variables for this function
     float strtNum, //starting number of organisms 
-            pDayInc, //average daily population increase as a percentage 
+            perInc, //avg. daily population inc. as a percentage 
+            numInc, //avg. daily population inc. as an integer 
             nForDay; //number of organisms for each day 
     int numDays; //number of days organisms multiplying 
     
@@ -30,20 +31,19 @@ int main(int argc, char** argv) {
     //Maps known values to the unknown objectives
     do { 
     cout << "Enter the initial number of organisms: "; //prompt for starting number 
-    cin << strtNum; //input for starting number 
+    cin >> strtNum; //input for starting number 
     cout << "Enter the average daily population increase (as a percentage): "; //prompt for population increase 
-    cin << pDayInc; //input for daily population increase 
+    cin >> numInc; //input for daily population increase in integer form 
     cout << "Enter the number of days the organisms will multiply: "; //prompt for days of multiplication 
-    cin << numDays; //input for number of days organisms multiplying 
-    } while (strtNum < 2 || pDayInc < 0 || numDays < 1); //do-while loop for input validation 
+    cin >> numDays; //input for number of days organisms multiplying 
+    } while (strtNum < 2 || perInc < 0 || numDays < 1); //do-while loop for input validation 
+    perInc = numInc / 100; //conversion pop. inc. integer to percentage 
         
     //Display the Inputs/Outputs 
     for (int dayCnt = 1; dayCnt <= numDays; dayCnt++) 
     { 
-        dayCnt == 1 ? nForDay = strtNum: 
-            nForDay = 
-        strtNum + (strtNum * pDayInc)
-        cout <<     ; //output for population size each day 
+        nForDay = strtNum + (strtNum * (perInc * dayCnt)); //daily population size 
+        cout << nForDay << endl; //output for daily population 
     }
 
     //Clean up the code, close files, deallocate memory, etc....
--- Projects/Review_Homework_1/Gaddis_9thEd_Chap6_Prob7_CelsiusTemperature/main.cpp
+++ Projects/Review_Homework_1/Gaddis_9thEd_Chap6_Prob7_CelsiusTemperature/main.cpp
@@ -7,6 +7,7 @@
 
 //System Libraries
 #include <iostream>  //I/O Library
+#include <iomanip>  //Format Library
 using namespace std;
 
 //User Libraries
@@ -15,23 +16,38 @@ using namespace std;
 //Well known Science, Mathematical and Laboratory Constants
 
 //Function Prototypes
+void celsius(float fDegree) { 
+    float cDegree = ((fDegree - 32) * 5 / 9); //equation to convert degrees C to F 
+    cout << setprecision(2) << fixed; //output formatting for decimal values 
+    cout << setw(16) << fDegree << setw(15) << cDegree << endl; //output for degrees conversion 
+} 
 
 //Execution of Code Begins Here
 int main(int argc, char** argv) {    
     //Declare all variables for this function
-    int fDegree, //degrees Fahrenheit 
-         cDegree; //degrees Celsius 
+    float fDegree, //degrees Fahrenheit 
+            cDegree; //degrees Celsius 
     
     //Initialize all known variables
     
     
     //Process Inputs to Outputs -> Mapping Process
-    //Maps known values to the unknown objectives
-    
+    //Maps known values to the unknown objectives 
+    cout << "Enter a degree in Fahrenheit: "; //prompt for degrees Fahrenheit 
+    cin >> fDegree; //input for degrees Fahrenheit 
+    cout << "The degree value has been converted "; //output for degrees conversion 
+    cout << "from Fahrenheit to Celsius: " << endl; //output for degrees conversion 
+    celsius(fDegree); //call for function celsius 
+    cout << endl; //space between initial conversion and conversion table 
+    cout << setw(30) << "Degree Conversion Table" << endl; //title of conversion table 
+    cout << setw(16) << "Fahrenheit" << setw(15) << "Celsius" << endl; //column titles for conversion table 
+    for (float cnt=0; cnt<=20; cnt++) { 
+        fDegree=cnt; //set degrees F to count value 
+        celsius(fDegree); //call for function celsius  
+    } 
     
     //Display the Inputs/Outputs 
     
-
     //Clean up the code, close files, deallocate memory, etc....
     //Exit stage right
     return 0;
